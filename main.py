b84c96f83c5299b888d6616a96f7010067fa22f4
Derek S <[serrand@mcmaster.ca]>
2021-09-25 12:45:20 -0400

    progress

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import kivy
-
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 kivy.require('1.9.0')
-
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
-class Crypto(GridLayout):
-    pass
 
+class Crypto(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(Crypto, self).__init__(**kwargs)
+        
+        # populate the RecycleView
+        self.ids.coinList.data = [{'value': str(i)} for i in range(20)]
 
 class CryptoApp(App):
 
@@ -16,6 +22,13 @@ class CryptoApp(App):
         # Return a label widget with Hello Kivy
         return Crypto()
         # return Label(text='Hello world')
+
+class InfoPage(App):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
 
 if __name__ == "__main__":
     cyrptoApp = CryptoApp()
