993857fc653240f7633f7479b7edf415266375f8
Derek S <[serrand@mcmaster.ca]>
2021-09-25 13:34:25 -0400

    more progress

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 
+class Crypto(Screen):
+        pass
+
 class Crypto(GridLayout):
 
     def __init__(self, **kwargs):
@@ -16,12 +19,17 @@ class Crypto(GridLayout):
         self.ids.coinList.data = [{'value': str(i)} for i in range(20)]
 
 class CryptoApp(App):
+
     # This returns the content we want in the window
     def build(self):
         # Return a label widget with Hello Kivy
         return Crypto()
         # return Label(text='Hello world')
 
+class InfoPage(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
 
 
 if __name__ == "__main__":
