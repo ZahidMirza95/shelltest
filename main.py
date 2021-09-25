a09cf5efb4483b3a8f60aa3911575fd04a64703a
Derek S <[serrand@mcmaster.ca]>
2021-09-25 16:56:53 -0400

    data trasnfer working

--- main.py
+++ main.py
@@ -5,29 +5,36 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 kivy.require('1.9.0')
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import StringProperty
+from kivy.clock import Clock
 
 
 class Crypto(Screen):
-        pass
+    def doSomething(instance,test):
+        print(test)
+    def update(self):
+        self.ids.buttonTwo.text="nice"
+
+
 class InfoPage(Screen):
     pass
 class WindowManager(ScreenManager):
     pass
-
-class Crypto(GridLayout):
+kv = Builder.load_file("Crypto.kv")
+'''class Crypto(GridLayout):
 
     def __init__(self, **kwargs):
         super(Crypto, self).__init__(**kwargs)
         
         # populate the RecycleView
-        self.ids.coinList.data = [{'value': str(i)} for i in range(20)]
+        self.ids.coinList.data = [{'value': str(i)} for i in range(20)]'''
 
-kv = Builder.load_file("Crypto.kv")
 class CryptoApp(App):
-
+    value=StringProperty('hello')
     # This returns the content we want in the window
     def build(self):
         # Return a label widget with Hello Kivy
+        
         return kv
         # return Label(text='Hello world')
 
