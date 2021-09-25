841bb03656ad5e5ed58346b4a8cf6727ce41cf01
Derek S <[serrand@mcmaster.ca]>
2021-09-25 13:37:07 -0400

    button working

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from kivy.uix.gridlayout import GridLayout
 
 class Crypto(Screen):
         pass
+class InfoPage(Screen):
+    pass
+class WindowManager(ScreenManager):
+    pass
 
 class Crypto(GridLayout):
 
@@ -18,18 +22,16 @@ class Crypto(GridLayout):
         # populate the RecycleView
         self.ids.coinList.data = [{'value': str(i)} for i in range(20)]
 
+kv = Builder.load_file("Crypto.kv")
 class CryptoApp(App):
 
     # This returns the content we want in the window
     def build(self):
         # Return a label widget with Hello Kivy
-        return Crypto()
+        return kv
         # return Label(text='Hello world')
 
-class InfoPage(Screen):
-    pass
-class WindowManager(ScreenManager):
-    pass
+
 
 
 if __name__ == "__main__":
